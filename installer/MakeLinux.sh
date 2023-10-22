cfbd40d29dcb1a77df2c8cece675fe26bb20e084
joaoelson <joaoelson@gmail.com>
2023-10-22 10:05:45 -0400

    Atualizando script de compilação linux e windows

--- installer/MakeLinux.sh
+++ installer/MakeLinux.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
+
 CPPBRASIL_PATH="../.."
-INSTALL_PATH=""
-QTRPT_HEADER="../../third-party/linux/QtRptProject3.0/include/QtRPT"
-QTRPT_LIB="../../third-party/linux/QtRptProject3.0/lib/debug"
 XML2_PATH="/usr/include/libxml2"
-
-rm -fr build
-mkdir build
-
-
-if [ -z $INSTALL_PATH ]; then
-    CMD_INSTALL=""
-else
-    CMD_INSTALL="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
-fi
-
-
-cd build
+QTRPT_HEADER="../../third-party/linux/QtRptProject3.0/include/QtRPT"
 
 echo "Informe o Diretorio do Qt onde se encontra o gcc_64: "
-read QT_GCC_PATH
+read  QT_GCC_PATH;
 
 if [ -z $QT_GCC_PATH ]; then
     CMD_PREFIX_PATH=""
@@ -27,26 +13,78 @@ else
     CMD_PREFIX_PATH="-DCMAKE_PREFIX_PATH=$QT_GCC_PATH"
 fi
 
+echo
+echo
+echo "Caminho para instalacao. Caso nao informe o padrao sera: /usr/"
+echo  "Informe o caminho para instalcao : "
+read INSTALL_PATH;
+
+if [ -z $INSTALL_PATH ]; then
+    CMD_INSTALL=""
+else
+    CMD_INSTALL="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+fi
+
+echo
+echo
+echo  "Informe o Tipo de Compilacao [D] para Debug [R] para Release:"
+read  BUILD_TYPE;
+
+if [ -z $BUILD_TYPE ] || [ $BUILD_TYPE  == "R" ] || [ $BUILD_TYPE  == "r" ]; then
+    CMD_BUILD_TYPE="-DCMAKE_BUILD_TYPE=Release"
+    QTRPT_LIB="../../third-party/linux/QtRptProject3.0/lib/release"
+else
+    CMD_BUILD_TYPE="-DCMAKE_BUILD_TYPE=Debug"
+    QTRPT_LIB="../../third-party/linux/QtRptProject3.0/lib/debug"
+
+fi
+
+
+rm -fr build
+mkdir build
+cd build
+
 
 #CPLUS_INCLUDE_PATH include para C++. Se for C é C_INCLUDE_PATH
 CPLUS_INCLUDE_PATH=$XML2_PATH:$QTRPT_HEADER:$CPLUS_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH
 
 CPPNFE=$CPPBRASIL_PATH"/CppBrasil/NFe"
-cmake -GNinja $CPPNFE $CMD_PREFIX_PATH $CMD_INSTALL
-ninja && sudo ninja install
+cmake -GNinja $CPPNFE $CMD_BUILD_TYPE $CMD_PREFIX_PATH $CMD_INSTALL
+#se a instalação for no /usr/ pede senha do sudo
+if [ -z $CMD_INSTALL ]; then
+    ninja && sudo ninja install
+else
+    ninja && ninja install
+fi
 
 rm -fr CMakeFiles CppNFe_autogen build.ninja CMakeCache.txt cmake_install.cmake install_manifest.txt
 
-sudo cp -R $QTRPT_HEADER /usr/include
-CPLIB="$QTRPT_LIB/libQtRPT.so"
-sudo cp $CPLIB /usr/lib
+#CPPDANFEQTRPT------------------------------
 
-echo $CXX_FLAGS
+#caso a instação seja em /usr/lib não haverá nenhuma flag para link das libs CppNFe e QtRPt, apenas copiará a libQtRPT para /usr/lib
+if [ -z $CMD_INSTALL ]; then
+    sudo cp -R $QTRPT_HEADER /usr/include
+    CPLIB="$QTRPT_LIB/libQtRPT.so"
+    sudo cp $CPLIB /usr/lib
+    LINKER_FLAG=""
+else
+#caso informe outro caminho para instação, haverá as flags para link das libs CppNFe e QtRPt. Também copiará a libQtRPT para o diretório informado
+    cp -R $QTRPT_HEADER "$INSTALL_PATH/include"
+    CPLIB="$QTRPT_LIB/libQtRPT.so"
+    cp $CPLIB "$INSTALL_PATH/lib"
+    LINKER_FLAG="-DCMAKE_SHARED_LINKER_FLAGS=-L$INSTALL_PATH/lib  "
+fi
+
+
 CPPDANFEQTRPT=$CPPBRASIL_PATH"/CppBrasil/DanfeQtRPT"
 
-cmake -GNinja $CPPDANFEQTRPT $CMD_PREFIX_PATH $CMD_INSTALL
-ninja && sudo ninja install
+cmake -GNinja $CPPDANFEQTRPT $CMD_BUILD_TYPE $CMD_PREFIX_PATH $LINKER_FLAG $CMD_INSTALL
+if [ -z $CMD_INSTALL ]; then
+    ninja && sudo ninja install
+else
+    ninja && ninja install
+fi
 
 cd ..
 
--- installer/MakeMSVC.bat
+++ installer/MakeMSVC.bat
@@ -3,10 +3,18 @@
 
 echo.
 echo.
-
 @echo off
 set /p QT_MSVC_PATH=Informe o caminho da pasta msvc2019_64 ou superior que se encontra na instalacao do Qt: 
-set INSTALL_PATH=..\..
+
+echo.
+set /p BUILD_TYPE=Informe o Tipo de Compilacao [D] para Debug [R] para Release:
+if [%BUILD_TYPE%]==[] set BUILD_TYPE=Release
+if [%BUILD_TYPE%]==[R] set BUILD_TYPE=Release
+if [%BUILD_TYPE%]==[r] set BUILD_TYPE=Release
+if [%BUILD_TYPE%]==[D] set BUILD_TYPE=Debug
+if [%BUILD_TYPE%]==[d] set BUILD_TYPE=Debug
+
+set INSTALL_PATH=..\..\build\%BUILD_TYPE%
 set CPPBRASIL_PATH=..\..\CppBrasil
 set OPENSSL_HEADER=..\..\third-party\windows\OpenSSL3.1.3\include
 set OPENSSL_LIB=..\..\third-party\windows\OpenSSL3.1.3\lib
@@ -17,17 +25,16 @@ set LIBICONV_LIB=..\..\third-party\windows\libiconv-for-Windows\lib64\Release
 set ZLIB_HEADER=..\..\third-party\windows\zlib-1.3\include
 set ZLIB_LIB=..\..\third-party\windows\zlib-1.3\lib
 set QTRPT_HEADER=..\..\third-party\windows\QtRptProject3.0\include\QtRPT
-set QTRPT_LIB=..\..\third-party\windows\QtRptProject3.0\lib\debug\lib
-
+set QTRPT_LIB=..\..\third-party\windows\QtRptProject3.0\lib\release\lib
+if [%BUILD_TYPE%]==[Debug] set QTRPT_LIB=..\..\third-party\windows\QtRptProject3.0\lib\debug\lib
 echo.
 echo.
-
 echo Informe o caminho do arquivo vcvars64.bat que se encontra na instalacao do Visual Studio
 echo Caso nao informe, o padrao sera: C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build 
 set /p PATH_VCVARS64=Informe o caminho do vcvars64.bat: 
 echo.
 
-if [%PATH_VCVARS64%]==[]  set PATH_VCVARS64=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
+if [%PATH_VCVARS64%]==[] set PATH_VCVARS64=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 
 set FILE_VCVARS64=%PATH_VCVARS64%\vcvars64.bat
 
@@ -39,17 +46,16 @@ cd build
 
 set INCLUDE_CMD= /I %OPENSSL_HEADER% /I %LIBXML2_HEADER% /I %LIBICONV_HEADER% /I %ZLIB_HEADER% /I %QTRPT_HEADER%
 set LIB_CMD= /LIBPATH:%OPENSSL_LIB% /LIBPATH:%LIBXML2_LIB% /LIBPATH:%LIBICONV_LIB% /LIBPATH:%ZLIB_LIB% /LIBPATH:%QTRPT_LIB%
-cmake -GNinja %CPPBRASIL_PATH%\NFe -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_MSVC_PATH% -DCMAKE_CXX_FLAGS=" %INCLUDE_CMD% " -DCMAKE_SHARED_LINKER_FLAGS=" %LIB_CMD% " -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%
+cmake -GNinja %CPPBRASIL_PATH%\NFe -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_PREFIX_PATH=%QT_MSVC_PATH% -DCMAKE_CXX_FLAGS=" %INCLUDE_CMD% " -DCMAKE_SHARED_LINKER_FLAGS=" %LIB_CMD% " -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%
 ninja
 ninja install
 
 rd /s /q CMakeFiles CppNFe_autogen 
 del build.ninja CMakeCache.txt cmake_install.cmake install_manifest.txt .ninja_deps .ninja_log
 
-cmake -GNinja %CPPBRASIL_PATH%\DanfeQtRPT -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_MSVC_PATH% -DCMAKE_CXX_FLAGS=" %INCLUDE_CMD% " -DCMAKE_SHARED_LINKER_FLAGS=" %LIB_CMD% " -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%
+cmake -GNinja %CPPBRASIL_PATH%\DanfeQtRPT -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_PREFIX_PATH=%QT_MSVC_PATH% -DCMAKE_CXX_FLAGS=" %INCLUDE_CMD% " -DCMAKE_SHARED_LINKER_FLAGS=" %LIB_CMD% " -DCMAKE_INSTALL_PREFIX=%INSTALL_PATH%
 ninja
 ninja install
-
 
 cd ..
 
